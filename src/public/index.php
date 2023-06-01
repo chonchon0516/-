6710349334fcbc4166acd939505e5c42ab65f7fd
chonchon0516 <azumi.toyoda@gmail.com>
2023-06-01 21:52:22 +0900

    add:トップページ作成

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,59 @@
 <?php
-echo 'Welcome TECH QUEST!';
+$dbUserName = 'root';
+$dbPassword = 'password';
+$pdo = new PDO(
+    'mysql:host=mysql; dbname=booksmanagement; charset=utf8',
+    $dbUserName,
+    $dbPassword
+);
+
+$sql = "SELECT * FROM books";
+$statement = $pdo->prepare($sql);
+$statement->execute();
+$pages = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($pages as $key => $value) {
+    $standard_key_array[$key] = $value['created_at'];
+}
+if(!empty($pages)){
+  array_multisort($standard_key_array, SORT_DESC, $pages);
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="./main.js" defer></script>
+    <link rel="stylesheet" href="./style.css">
+</head>
+  <body>
+    <header class="header">
+      <a href="./create.php" class="new_blog">書籍を追加</a>
+    </header>
+  <div>
+    <table border="1">
+      <tr>
+        <th>タイトル</th>
+        <th>感想</th>
+        <th>作成日時</th>
+        <th>編集</th>
+        <th>削除</th>
+      </tr>
+
+      <?php foreach ($pages as $page): ?>
+        <tr>
+          <td><?php echo $page['title']; ?></td>
+          <td><?php echo $page['content']; ?></td>
+          <td><?php echo $page['created_at']; ?></td>
+          <td><a href="./edit.php?id=<?php echo $page['id']; ?>">編集</a></td>
+          <td><a href="./delete.php?id=<?php echo $page['id']; ?>">削除</a></td>
+        </tr>
+      <?php endforeach; ?>
+
+    </table>
+  </div>
+
+</body>
